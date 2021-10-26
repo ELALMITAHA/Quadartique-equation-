68fd3b927b9dcd1aada2d34cf0083735bee66405
EL ALMI TAHA <taha.elalmi@yahoo.com>
2021-10-26 08:07:10 +0200

    add function to solve 2d equation

--- quadsolove.py
+++ quadsolove.py
@@ -39,18 +39,18 @@ class SolveQuad(QWidget):
 		c_lbl  = QLabel("c")
 
 		# To get coefficient from the user 
-		a_linedit = QLineEdit()
-		b_linedit = QLineEdit()
-		c_linedit = QLineEdit()
+		self.a_linedit = QLineEdit()
+		self.b_linedit = QLineEdit()
+		self.c_linedit = QLineEdit()
 
 		#Add widget to group 
 		grid_layout.addWidget(a_lbl,0,0)
 		grid_layout.addWidget(b_lbl,0,1)
 		grid_layout.addWidget(c_lbl,0,2)
 
-		grid_layout.addWidget(a_linedit,1,0)
-		grid_layout.addWidget(b_linedit,1,1)
-		grid_layout.addWidget(c_linedit,1,2)
+		grid_layout.addWidget(self.a_linedit,1,0)
+		grid_layout.addWidget(self.b_linedit,1,1)
+		grid_layout.addWidget(self.c_linedit,1,2)
 
 		#set Layout for Input group 
 		input_grp.setLayout(grid_layout)
@@ -59,15 +59,43 @@ class SolveQuad(QWidget):
 		solve_btn = QPushButton(" Solve ")
 		solve_btn.clicked.connect(self.solve2dEquation)
 
+		#set solution 
+
+		self.solution = QLabel("")
+
 		# Set layout for main window  
 		glbl_box  = QVBoxLayout() 
 		glbl_box.addWidget(title_lbl)
 		glbl_box.addWidget(input_grp)
 		glbl_box.addWidget(solve_btn)
+		glbl_box.addWidget(self.solution)
 		self.setLayout(glbl_box)
 	
-	def solve2dEquation(self) : 
-		pass 
+	def solve2dEquation(self) :  
+		try : 
+			a = float(self.a_linedit.text())
+			b = float(self.b_linedit.text())
+			c = float(self.c_linedit.text())
+		except ValueError : 
+			self.solution.setText("You must give a numerical value !!")
+
+		if a == 0 : 
+			self.solution.setText(" a must not equal to 0 !!")
+
+		delta = b**2 - 4*a*c
+
+		if delta < 0 : 
+			self.solution.setText(" Pas de solutions reelles ")
+		elif delta == 0:
+			x = -b/(2*a)
+			x = str(x)
+			self.solution.setText("solution unique : " +  x ) 
+		else : 
+			x1 = (-b - math.sqrt(delta))/(2*a)
+			x1 = str(x1)
+			x2 = str((-b + math.sqrt(delta))/(2*a))
+			x2 = str(x2)
+			self.solution.setText(f'x1 =  {x1}  ;   x2 = {x2}' )
 
 
 if __name__ == "__main__": 
